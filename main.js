aa2f993fd813ba30b6640f783a90d50ba4e55c13
Harshit Raj <harshitraj712@gmail.com>
2020-10-08 20:03:48 +0530

    added upper obstacle and scorecard

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ document.addEventListener('DOMContentLoaded',() =>{
 
 let bird = document.querySelector('#bird')
 let ground = document.querySelector('#ground')
+let scoreCard = document.querySelector('#score')
 let birdHeight = 100
 let birdLeft = 300
 let gameOver = true
 let score = 0
 let birdTimer
-
+let gap = 110
 
 document.addEventListener('keyup',control)
 
@@ -42,38 +43,52 @@ function control(e){
 
 function createObstacle() {
   let obstacleleft = 700
-  let obHeight = Math.random() * 310
+  let obHeight = Math.random() * 290
+  let top = 400 - obHeight - gap
   let obstacle = document.createElement('div')
+  let topobstacle = document.createElement('div')
   obstacle.className += "obstacle"
+  topobstacle.className += "topobstacle"
   obstacle.style.left = obstacleleft + 'px'
+  topobstacle.style.left = obstacleleft + 'px'
   obstacle.style.height = obHeight + 'px'
+  topobstacle.style.height = top + 'px'
+  topobstacle.style.bottom = (100 + obHeight + gap) + 'px'
   ground.appendChild(obstacle)
-
+  ground.appendChild(topobstacle)
+  let topbottom = obHeight + gap - 30
   function moveOb() {
-    obstacleleft -= 2
+    obstacleleft -= 4
     obstacle.style.left = obstacleleft + 'px'
+    topobstacle.style.left = obstacleleft + 'px'
     if (obstacleleft == 240){
       score += 1
       console.log(score)
+      scoreCard.innerHTML = `Score: ${score}`
 
     }
-    if(obstacleleft == 130){
+    if(obstacleleft == 128){
       createObstacle()
     }
     if(obstacleleft <= -50){
       clearInterval(obMove)
       obstacle.remove()
+      topobstacle.remove()
     }
 
-    if(birdHeight <= 0||birdHeight <= obHeight && obstacleleft <= 330 &&
-    obstacleleft >= 250){
+    if(birdHeight <= -2||birdHeight <= obHeight&& obstacleleft <= 330 &&
+    obstacleleft >= 240||birdHeight >= topbottom && obstacleleft <= 330 &&
+    obstacleleft >= 240)
+    {
       gameOver = true
+      console.log(birdHeight)
+      console.log(obHeight)
+      console.log(bird.style.bottom)
       clearInterval(obMove)
       clearInterval(birdTimer)
     }
   }
-  let obMove = setInterval(moveOb,10)
-
+  let obMove = setInterval(moveOb,20)
 }
 
 
